4e3469c7584b6d2736c70448cf3085f8a702fc88
Abyel <abyel26@gmail.com>
2020-02-02 15:06:47 -0700

    adding project with googlebooks.js

---
+++ utils/googlebooks.js
@@ -0,0 +1,32 @@
+ const axios = require('axios')
+
+    module.exports = async (phrase) => {
+        const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+            params: {
+                format: 'json',
+                q: `${phrase}`
+            }
+        })
+        return JSON.stringify(results.data);
+    }
+
+ async function f() {
+     const results = await axios({
+         method: 'get',
+         url: 'https://www.googleapis.com/books/v1/volumes',
+         params: {
+             format: 'json',
+             q: `${phrase}`,
+         },
+     })
+
+     console.log(results.data);
+     console.log(results.status);
+     console.log(results.statusText);
+     console.log(results.headers);
+     console.log(results.config.headers);
+ }
+
+
+
+
